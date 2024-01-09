94cd5160fed93bf3ec757b39aa26659b99b1bf7d
Kristián Botek <145974203+Kristianbotek@users.noreply.github.com>
2024-01-09 21:42:27 +0100

    Add files via upload

---
+++ Kalkulačka - Botek/Procvicovani-if/source.js
@@ -0,0 +1,21 @@
+function loginFun() {
+
+    const database = ["pepik.ahoj1","jana.pizza2", "karel.kočka3", "eva.12345"]
+
+    let username = document.getElementById("loginUsername").value
+    let password = document.getElementById("loginPassword").value
+
+    let loginInfo = username + "." + password;
+    console.log(loginInfo);
+
+    for (let i = 0; i < database.length; i++) {
+        if (loginInfo === database[i]) {
+            document.getElementById("loginText").innerHTML = "Přihlášen";
+            window.location.href = "index2.html";
+            return;
+    
+    
+        }
+    }
+document.getElementById("loginText").innerHTML = "ŠPATNÉ PŘIHLAŠOVACÍ ÚDAJE";
+}
